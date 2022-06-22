3e039ef0887cc9e228d1806726baa13d55204fdc
Omar Bafagih <bafagihomar260@gmail.com>
2022-06-22 13:05:12 -0700

    Users can now login

--- app/src/main/java/com/example/healthinspector/LoginActivity.java
+++ app/src/main/java/com/example/healthinspector/LoginActivity.java
@@ -2,16 +2,107 @@ package com.example.healthinspector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    public static final String TAG = "LoginActivity";
+    private Button btnLogin, btnSignup;
+    private EditText editTextUsername, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        
+
+        //if the user was already previously logged in
+        if(ParseUser.getCurrentUser() != null){
+            navigateToHome();
+        }
+
+        //getting references to XML elements
+        editTextUsername = (EditText) findViewById(R.id.etUsername);
+        editTextPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnSignup = (Button) findViewById(R.id.btnSignup);
+
+        //creating on click listeners for login and signup buttons
+        //Login button onClick listener
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick Login button");
+
+                //collect the inputted text from the input fields
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                //simple input checking
+                if(username.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please enter your username", Toast.LENGTH_SHORT).show();
+                }
+                else if(password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "please enter your password", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    loginUser(username, password);
+                }
+
+            }
+        });
+
+        //Login button onClick listener
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick Signup button");
+
+               //go to "user profile" fragment where user can add their allergies/ingredients etc...
 
 
+            }
+        });
+
+
+
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user: " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "error with login: ", e);
+                    Toast.makeText(LoginActivity.this, "Login information incorrect", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //navigate to Timeline activity on successful login
+                navigateToHome();
+                Toast.makeText(LoginActivity.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+    }
+
+
+
+
+    public void navigateToHome(){
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(i);
     }
 }
