d358b9d7912b5e44cea1507f222a625957c8af7f
Omar Bafagih <bafagihomar260@gmail.com>
2022-06-24 16:05:16 -0700

    Removed unnecessary comment

--- app/src/main/java/com/example/healthinspector/Fragments/ScanFlow/ProductDetailsFragment.java
+++ app/src/main/java/com/example/healthinspector/Fragments/ScanFlow/ProductDetailsFragment.java
@@ -27,7 +27,6 @@ public class ProductDetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         binding = FragmentProductDetailsBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         return view;
