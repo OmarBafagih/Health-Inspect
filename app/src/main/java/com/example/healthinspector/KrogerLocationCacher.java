cf75a5b83a9901e3445a76595047418160d4bbc5
Omar Bafagih <bafagihomar260@gmail.com>
2022-07-09 07:35:28 -0700

    Created Handler for API requests that caches closest locations to user
    - Requests location permissions

---
+++ app/src/main/java/com/example/healthinspector/KrogerLocationCacher.java
@@ -0,0 +1,138 @@
+package com.example.healthinspector;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.util.Log;
+
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class KrogerLocationCacher extends Application{
+    private static final String TAG = "KrogerLocationCacher";
+    private static KrogerLocationCacher krogerLocationCacher = null;
+    private String token = null;
+    private ArrayList<JSONObject> krogerLocations = null;
+    private static final String ACCESS_TOKEN = "access_token";
+
+    private KrogerLocationCacher() {}
+    public static KrogerLocationCacher getInstance() {
+        if (krogerLocationCacher == null){
+            krogerLocationCacher = new KrogerLocationCacher();
+        }
+        return krogerLocationCacher;
+    }
+
+    public String getToken(Context context){
+        if(token == null){
+            token = makeTokenRequest(context);
+        }
+        return token;
+    }
+
+    public ArrayList<JSONObject> getNearbyKrogerLocations(Double latitude, Double longitude, Context context){
+        if(krogerLocations == null){
+            krogerLocations = makeLocationRequest(latitude, longitude, context);
+        }
+        return krogerLocations;
+    }
+
+
+    public String makeTokenRequest(Context context){
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials&scope=product.compact");
+        Request request = new Request.Builder()
+                .url("https://api.kroger.com/v1/connect/oauth2/token")
+                .post(body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .addHeader("Authorization", "Basic " + context.getString(R.string.kroger_key))
+                .build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+                try {
+                    //TOKEN RETRIEVAL SUCCESSFUL
+                    JSONObject jsonResponse = new JSONObject(response.body().string());
+                    token = jsonResponse.getString(ACCESS_TOKEN);
+                    Log.i(TAG, token);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return token;
+    }
+
+
+    public ArrayList<JSONObject> makeLocationRequest(Double latitude, Double longitude, Context context) {
+        ArrayList<JSONObject> locations = new ArrayList<>();
+        // create api request to get json locations
+        OkHttpClient locationsRequestClient = new OkHttpClient();
+
+        Request requestLocations = new Request.Builder()
+                .url("https://api.kroger.com/v1/locations?filter.latLong.near=" + (latitude) + "," + longitude)
+                .get()
+                .addHeader("Accept", "application/json")
+                .addHeader("Authorization", "Bearer " + getToken(context))
+                .build();
+
+
+        locationsRequestClient.newCall(requestLocations).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "ERROR RETRIEVING LOCATIONS", e);
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                JSONObject jsonResponse = null;
+                try {
+                    jsonResponse = new JSONObject(response.body().string());
+                    for(int i = 0; i < jsonResponse.getJSONArray("data").length(); i++){
+                        JSONObject location = new JSONObject();
+                        location.put(Constants.LOCATION_ID, jsonResponse.getJSONArray(Constants.DATA).getJSONObject(i).getString(Constants.LOCATION_ID));
+                        location.put(Constants.STORE_NAME, jsonResponse.getJSONArray(Constants.DATA).getJSONObject(i).getString("name"));
+                        location.put(Constants.ADDRESS, jsonResponse.getJSONArray(Constants.DATA).getJSONObject(i).getJSONObject(Constants.ADDRESS).getString("addressLine1"));
+                        location.put(Constants.LATITUDE, jsonResponse.getJSONArray(Constants.DATA).getJSONObject(i).getJSONObject("geolocation").getDouble(Constants.LATITUDE));
+                        location.put(Constants.LONGITUDE, jsonResponse.getJSONArray(Constants.DATA).getJSONObject(i).getJSONObject("geolocation").getDouble(Constants.LONGITUDE));
+                        locations.add(location);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.i(TAG, jsonResponse.toString());
+            }
+        });
+        return locations;
+    }
+}
